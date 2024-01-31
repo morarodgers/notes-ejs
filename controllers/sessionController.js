aa8a8d9dd17d89e18a6a87cfa6c063ce8faf8498
morarodgers <morarodgers@gmail.com>
2024-01-31 16:52:49 +0300

    Lesson 13 corrections

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -8,7 +8,7 @@ const registerShow = (req, res) => {
 const registerDo = async (req, res, next) => {
   if (req.body.password != req.body.password1) {
     req.flash("error", "The passwords entered do not match.");
-    res.render("register");
+    res.render("register",{errors: req.flash("error")});
   }
   try {
     await User.create(req.body);
@@ -20,7 +20,7 @@ const registerDo = async (req, res, next) => {
     } else {
       return next(e);
     }
-    return res.render("register");
+    return res.render("register",{errors: req.flash("error")});
   }
   res.redirect("/");
 };
